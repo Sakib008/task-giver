e974bf207cf8edbc2706cdc2293c2b1c95b8d647
Sakib008 <mdshakib1902@gmail.com>
2025-08-23 21:43:48 +0530

    feat : added db

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const express = require('express')
+const initializeDatabase = require('./db/db.connect')
+
 const app = express();
+initializeDatabase();
 
 app.get("/",(req,res)=>{
     res.send("Hello agent task app is working fine")
